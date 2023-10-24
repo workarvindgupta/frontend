a6ea0f0a235f2d5819c2778995f037c0ab1b29d9
arvind <work.arvindgupta@gmail.com>
2023-10-24 23:07:33 +0530

    pratice

---
+++ javascript/practice/1.js
@@ -0,0 +1,19 @@
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+
+const number = 5;
+const square = number * number;
+console.log(square); // Output: 25
+
+
+const numbers = [4, 9, 16, 25];
+const newArr = numbers.map(Math.sqrt);
+
+console.log(newArr);
