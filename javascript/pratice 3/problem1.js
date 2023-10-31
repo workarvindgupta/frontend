16f510ea59b84cae0f76e374b21e128f01b944ef
arvind <work.arvindgupta@gmail.com>
2023-10-31 23:36:27 +0530

    pratice 3

---
+++ javascript/pratice 3/problem1.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program to check whether a given integer is within 20 of 100 or 400.
+
+
+function testhundred(x) {
+    return ((Math.abs(100 - x) <= 20) ||
+       (Math.abs(400 - x) <= 20));
+  }
+  
+  console.log(testhundred(10));
+  console.log(testhundred(90));
+  console.log(testhundred(99));
+  console.log(testhundred(199));
+  console.log(testhundred(200));
+  
---
+++ javascript/pratice 3/url.js
@@ -0,0 +1,5 @@
+// Write a JavaScript program to get the website URL (loading page).
+
+
+//Write a JavaScript program to get the website URL (loading page)
+console.log(document.URL);
