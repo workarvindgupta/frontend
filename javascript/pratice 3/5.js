182719e485c35a21289f4d86a01819c63397dc60
arvind <work.arvindgupta@gmail.com>
2023-11-04 23:39:13 +0530

    4 5

---
+++ javascript/pratice 3/5.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to reverse a given string.
+
+
+function string_reverse(str) 
+{
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("w3resource"));
+console.log(string_reverse("www"));
+console.log(string_reverse("JavaScript"));
